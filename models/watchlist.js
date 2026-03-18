610c7e3176ab0a4f27fda7767ab61466335de710
LE4FEX <thanapon.choy@gmail.com>
2026-03-18 10:05:24 +0000

    Deleted file: models/watchlist.js

--- models/watchlist.js
+++
@@ -1,8 +0,0 @@
-const mongoose = require('mongoose');
-
-const watchlistSchema = new mongoose.Schema({
-    userId: { type: String, required: true, unique: true },
-    symbols: { type: [String], default: [] }
-});
-
-module.exports = mongoose.model('Watchlist', watchlistSchema);
